0fed15bb5bbe46f54bf641dfa04e496dd62d2451
DANIELGESHE <114051211+DANIELGESHE@users.noreply.github.com>
2022-10-06 08:28:23 +0300

    Add files via upload
    A while loop program to display even numbers between 0-100

---
+++ while loop even Numbers 0-100.cpp
@@ -0,0 +1,15 @@
+// program using the while loop to dispay even numbers between 0-100
+#include <iostream>
+using namespace std;
+int main ()
+{
+//iniatialising the variable
+int i=2;
+//condition 
+while (i<=100)
+{
+    cout <<i<<" ";
+    i=i+2;
+}
+return 0;
+}
