726bbbd10592dd71b401f51ee98c727a82ded615
DANIELGESHE <114051211+DANIELGESHE@users.noreply.github.com>
2022-10-06 09:04:20 +0300

    Add files via upload
    A do...while loop program to display numbers ranging from 10-1

---
+++ do while 10-1.cpp
@@ -0,0 +1,17 @@
+//program using the do..while loop to display numbers from 10-1
+#include <iostream>
+using namespace std;
+int main ()
+{
+//initialising the variable
+int i=10;
+ do 
+ {
+    cout <<i<<" ";
+    i--;
+ }
+ //condition
+    while (i>=1);
+//program terminates when the condition becomes false
+return 0;
+}
