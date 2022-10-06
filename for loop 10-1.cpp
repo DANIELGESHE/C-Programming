3f3ef025aa6bfaedcec626699ae7f6f6b2b35c10
DANIELGESHE <114051211+DANIELGESHE@users.noreply.github.com>
2022-10-06 09:02:50 +0300

    Add files via upload
    A for loop program to display numbers ranging from 10-1

---
+++ for loop 10-1.cpp
@@ -0,0 +1,13 @@
+// program using the for loop to display numbers from 10-1
+#include <iostream>
+using namespace std;
+int main ()
+{
+//variable declaration
+int i;
+for (i=10;i>=1;i--)
+{
+    cout <<i<<" ";
+}
+return 0;
+}
